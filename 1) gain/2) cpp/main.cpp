4127437c126dd7e1deff773c7820015a2e6d3b81
boe <boeboebeo@gmail.com>
2026-07-23 02:58:17 +0900

    feat: Object & Class

--- 1) gain/2) cpp/main.cpp
+++ 1) gain/2) cpp/main.cpp
@@ -1,6 +1,10 @@
 // main.cpp : 실행 및 테스트를 담당 (Implementation. 구현)
 
-// std:: -> standard(표준 라이브러리)
+// std:: -> standard(표준 라이브러리) <- std::__ 여기 자리에 붙는건 namespace. ex. std::sin()
+// cmath , random 은 헤더 이름! 
+// std::vector는 vector 라는 헤더이름에서 vector 라는 기능을 사용함
+// std::sin()는 cmath 라는 헤더이름에서 sin() 기능을 사용함
+// std::max(), juce::jmax(), myMath::max() 처럼 앞에 서로 다른 네임스페이스 안에 존재함
 
 
 // (1) 라이브러리 (include): import numpy as np 와 동일한 구조
@@ -25,11 +29,11 @@
 // == create_sine(fs, freq, duration): .. return 과 완전히 동일하다
 
 std::vector<float> createSine (double sampleRate, double frequency, double durationSeconds)
-//std::vector<float>을 반환 타입으로 쓰는 이유 : Python의 numpy array 에 대응하는 
+//std::vector<float>이 반환 타입 -> vector<float> 쓰는 이유 : Python의 numpy array 에 대응하는 
 //C++ 표준 라이브러리의 "크기가 자유롭게 늘어나는" 배열
 //std::vector<float> : float 만 저장하는 vector. python 은 여러 문자열을 같이 저장할 수 있지만 여기서는 안됨
 //c++ 에서는 무조건 자료형을 같이 적어줘야 함 ex. double sampleRate (python 에서는 알아서 다 판단했었다)
-// double? : 
+// double? : 소수/실수형 (좀더 많은 자리수까지 표현)
 
 {
     const int numSamples = static_cast<int> (sampleRate * durationSeconds);
@@ -44,21 +48,94 @@ std::vector<float> createSine (double sampleRate, double frequency, double durat
         // static : 정적인, 안전한 , cast : 형 변환(type conversion)
         // double : 소수(실수)를 저장하는 자료형. float 보다 더 높은 precision(정밀도)를 가짐
 
-    for (int i = 0; i < numSamples; ++i)
+    for (int i = 0; i < numSamples; ++i)    // 여기 ++i 가 1씩 키워줌 (반복후 1키움)
+                                            // i = i + 1 (= ++i)
+        // for 문의 구조는 for (초기화; 조건; 증가)
+        // { 실행 }
     {
-        const double t = static_cast<double> (i) / sampleRate;
+        const double t = static_cast<double> (i) / sampleRate; // 현재의 샘플 번호 i 를 double 형으로 바꿔서 /48000 함 -> time 계산
         buffer[static_cast<size_t> (i)] = static_cast<float> (std::sin (2.0 * M_PI * frequency * t));
+            //buffer[i] : buffer vector 에 접근
+            //size_t = 배열의 인덱스 전용 자료형
+            // 여기서는 buffer 라는 vector의 index 값의 자리에다가 sine 계산해서 진폭값 저장
+            // M_PI = pi (3.141592....)
     }
+
+    return buffer; 
+    // buffer 라는 vector 안에 각 t에서 계산된 사인파의 진폭값이 순서대로 저장되어 있음
+}
+
+
+// (2) 함수 정의_createWhiteNoise() 
+std::vector<float> createWhiteNoise(double sampleRate, double durationSeconds)
+{
+    const int numSamples = static_cast<int> (sampleRate * durationSeconds);
+    std::vector<float> buffer (static_cast<size_t> (numSamples));
+
+    std::random_device rd;
+        // random device : 매번 다른 값을 하나 가져옴 (랜덤한 seed 값)
+        // ex. 84372918, 1234 ..등의 값을 가지고 옴 
+        // random_device 라는 클래스(설계도)로 만든 객체(변수) : rd
+    std::mt19937 engine (rd()); 
+        // Mersenne Twister : mt19937. random 값 만들어내는 엔진
+        // mt19937 engine(1234); <- 위의 rd; 에서 가지고 온 값 
+    std::uniform_real_distribution<float> distribution (-1.0f, 1.0f);
+        // 어떤 범위의 Random 값을 만들 것인지 여기서는 -1.0 ~ 1.0 범위
+        // 결과를 float 형으로 만듦
+
+    for (int i = 0; i < numSamples; ++i) //초기화; 조건; 반복 후 마다 +1추가
+    buffer[static_cast<size_t> (i)] = distribution (engine);
+        // buffer[i] 의 인덱스 위치에 distribution(engine) 계산해서 넣음
+        // distribution : 함수처럼 동작하는 객체 (Function Object)
+        // 총 흐름 : engine 에게 random 값 하나 뽑아달라고 시키고, 그 결과를 distribution 규칙(-1~1)에 맞게 변환해서 반환한다.
 
     return buffer;
 }
 
 
-// (2) 함수 정의_createWhiteNoise() 
-
 
 
 // (3) Gain 적용 함수_applyGainAndSave() : def apply_gain(): 과 동일한 구조
+// void 는 반환하는 값이 없다는 뜻 ex. int add(...) 면 int 값을 반환하는데, void는 출력만하고 return;할 값 없음
+// Wave 파일 저장만 하고, 어떤 값을 돌려주지는 않아서 void 사용한 것 
+void applyGainAndSave (const std::vector<float>& inputWave, //const : 값 못 바꾸므로 곧 읽기 전용이라는 뜻, 입력버퍼 보호를 위함
+                                                // & inputWave : &(참조) -> inputWave 벡터 전체를 그대로 사용(복사는 오래걸리므로)
+                                                // & + const : 원본을 복사없이 읽기만 함 (복사못함)
+                        const std::string& waveName, //이름도 그대로 빌려옴
+                        float linearMagnitude,
+                        bool invert, // 여기서는 위상반전 따로 적용
+                                     // true: 위상반전, false: 그대로 출력
+                        double sampleRate)
+
+        // = 입력 오디오버퍼(inputWave)를 그대로 읽어와서 이름 저장, magnitude만큼 게인 적용,
+        // sampleRate 를 이용해서 처리 및 저장하는 함수다.  
+        // python 에서의 들여쓰기 구조가, 여기서는 {} 이렇게 표현됨 : 이 {}안이 함수 내부가 되는것.
+{
+    dsp::Gain gain;
+        //dsp:: - 어디에 있는 클래스 인지
+        //Gain : 설계도(Class). Gain.h 에 만들 설계도. Gain.h 에 gain 이라는 객체가 정의되어야 한다.
+        //gain : 객체 이름(변수). Gain 이라는 설계도로 만든 gain 이라는 객체를 하나 만듦.
+    gain.prepare (sampleRate);
+        /* Gain 이라는 클래스 안에 이런 함수들이 들어있음. prepare 은 함수 _.prepare : prepare 함수 실행
+        class Gain
+        {
+        public:
+
+            void prepare(...);
+
+            void setGainLinear(...);
+
+            float processSample(...);
+
+        };
+        */
+    gain.setGainLinear (linearMagnitude);
+
+    std::vector<float> output (inputWave.size());
+    float peakAbsValuse = 0.0f;
+
+
+}
 
 
 
@@ -102,5 +179,33 @@ return buffer
 
 => 계산은 double(정확도), 저장은 float(메모리와 성능)
 
-! 참고로 int 는 정수 
+! 참고로 int 는 정수 (샘플개수, 인덱스 처리등에 쓰임)
+! char     // 거의 안 쓰지만 파일 처리 등에서 사용 (문자 한개)
+*/
+
+/* C++ 에서의 객체
+
+    std::random_device rd;
+    std::mt19937 engine (rd());
+    std::uniform_real_distribution<float> distribution (-1.0f, 1.0f);
+ 
+    for (int i = 0; i < numSamples; ++i)
+        buffer[static_cast<size_t> (i)] = distribution (engine);
+ 
+    return buffer;
+
+
+
+    => 위 코드에서의 rd, rd(), distribution(engine) 
+    : 함수인것 같지만 사실상 함수가 아니고 객체이다.
+
+    ex. x.append(4) 는 리스트가 append 라는 기능을 가지고 있는것이다. 
+    ex. rng = random.Random() <- 여기서 rng 도 함수가 아니다.
+        => rng.randint(1,10) : 이렇게 사용할 수 있다.
+
+    **함수는 내부에 기억하는 상태(state) 가 없음
+    -> 그치만 함수가 아닌 객체여도 () 를 붙이면 이 객체가 이렇게 행동한다를 정의 할 수 는 있음
+
+1) 객체 (Object) : 데이터와 그 데이터를 다루는 기능을 함께 가진것
+2) 함수 (Function) : 입력을 받아 계산해서 결과를 돌려주는 코드
 */
