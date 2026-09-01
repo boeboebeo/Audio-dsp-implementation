df8678d096cb98a3a3785573a328b092f225badc
boe <boeboebeo@gmail.com>
2026-09-02 02:46:47 +0900

    feat : plot_impulse response

--- 3_biquad_eq/1_python/biquad_eq_old2.py
+++ 3_biquad_eq/1_python/biquad_eq_old2.py
@@ -2,6 +2,9 @@
 #여기서는 실제로 파라미터로 coefficients 를 도출해내서 실행해보자
 
 #2nd-order low-pass Biquad
+#bi-quadratic : 2차 구조를 가진 필터 
+    #거의 DSP에서는 2차 IIR 필터를 가리킴
+    #2차 : 최대 2샘플 전까지의 상태를 사용하는 구조
 """
 Low-pass
    ↓
@@ -24,29 +27,34 @@ a1/a2와 pole 관계 확인
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
-    f0 = 1000       #cutoff freq
-    Q = 0.707         #Quality factor (Resonance) => Q를 올리면 pole 이 unit circle 쪽으로 올라가는것 확인 가능
+    f0 = 10000       #cutoff freq
+    Q = 3       #Quality factor (Resonance) => Q를 올리면 pole 이 unit circle 쪽으로 올라가는것 확인 가능
     fs = 48000      #sample rate
     gain = 6        #gain (dB)
 
-    input_signal = np.zeros(fs)
+    #impulse로 test 
+    input_signal = np.zeros(fs) 
     input_signal[0] = 1
 
     b0, b1, b2, a1, a2 = calculate_lowpass_coefficients(
                             f0, Q, fs
                             )
+    
     output = biquad_filter_basic(
                             input_signal,
                             b0, b1, b2, a1, a2
     )
+
+    plot_impulse_response(output, fs)
     
 
 
 
 #계수 계산 함수
-
 def calculate_lowpass_coefficients(f0, Q, fs):
 
     # 1. omega0 계산
@@ -85,6 +93,51 @@ def calculate_lowpass_coefficients(f0, Q, fs):
 
 
 # 필터 적용 함수
-
 def biquad_filter_basic(input_signal, b0, b1, b2, a1, a2):
-    
+    #각 샘플 계산
+
+    #x, y 값 초기화 (for문 밖에서)
+    x1 = 0.0
+    x2 = 0.0
+    y1 = 0.0
+    y2 = 0.0
+
+    #input 길이 만큼의 output 배열 생성
+    output = [0.0] * len(input_signal)
+
+    for n, x0 in enumerate(input_signal):
+        y0 = b0*x0 + b1*x1 + b2*x2 - a1*y1 - a2*y2
+
+        output[n] = y0
+
+        #state update
+        x2 = x1
+        x1 = x0
+        y2 = y1
+        y1 = output[n]
+
+    return output
+
+
+#impulse response check 함수
+def plot_impulse_response(output, fs):
+    spectrum = np.fft.rfft(output)
+    magnitude = np.abs(spectrum)
+    mag_db = 20 * np.log10(magnitude + 1e-12)
+
+    #fft 간격
+    fft_freq = np.fft.rfftfreq(len(output), 1/fs)
+
+    plt.plot(fft_freq[1:], mag_db[1:])
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Amplitude (dB)")
+    plt.xscale("log")
+    plt.xlim(20, 40000)
+    plt.ylim(-60, 20)
+    plt.tight_layout()
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
