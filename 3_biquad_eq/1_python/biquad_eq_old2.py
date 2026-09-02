950fbe5175ac6c01e75f503659701f2eed419033
boe <boeboebeo@gmail.com>
2026-09-03 03:22:43 +0900

    feat : BPF 1, 2 comparison

--- 3_biquad_eq/1_python/biquad_eq_old2.py
+++ 3_biquad_eq/1_python/biquad_eq_old2.py
@@ -31,8 +31,8 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    f0 = 10000       #cutoff freq
-    Q = 3       #Quality factor (Resonance) => Q를 올리면 pole 이 unit circle 쪽으로 올라가는것 확인 가능
+    f0 = 1000      #cutoff freq
+    Q = 5       #Quality factor (Resonance) => Q를 올리면 pole 이 unit circle 쪽으로 올라가는것 확인 가능
     fs = 48000      #sample rate
     gain = 6        #gain (dB)
 
@@ -44,17 +44,40 @@ def main():
                             f0, Q, fs
                             )
     
-    output = biquad_filter_basic(
+    b0, b1, b2, a1, a2 = calculate_highpass_coefficients(
+                            f0, Q, fs
+    )
+
+    b0, b1, b2, a1, a2 = calculate_bandpass_coefficients(
+                            f0, Q, fs
+    )
+        #만약 Q값이 5라면 
+        #20log(5) = 14dB 정도 중심주파수가 증폭됨
+
+    output1 = biquad_filter_basic(
                             input_signal,
                             b0, b1, b2, a1, a2
     )
 
-    plot_impulse_response(output, fs)
+    b0, b1, b2, a1, a2 = calculate_bandpass2_coefficients(
+                            f0, Q, fs
+    )
+        #Q값과 관계없이, 중심주파수가 0dB (1배 = 조절없음)
+    
+    output2 = biquad_filter_basic(
+                            input_signal,
+                            b0, b1, b2, a1, a2
+    )
+
+    comparison_BPF1_BPF2(output1, output2, fs)
+
+    # plot_impulse_response(output, fs)
     
 
 
 
 #계수 계산 함수
+#1. low-pass
 def calculate_lowpass_coefficients(f0, Q, fs):
 
     # 1. omega0 계산
@@ -89,6 +112,77 @@ def calculate_lowpass_coefficients(f0, Q, fs):
     b2 = b2 / a0
 
     # 6. coefficient 반환
+    return b0, b1, b2, a1, a2
+
+
+#2. High-pass 
+def calculate_highpass_coefficients(f0, Q, fs):
+    w0 = 2 * np.pi * (f0/fs)
+
+    alpha = np.sin(w0) / (2 * Q)
+
+    b0 = (1 + np.cos(w0)) / 2
+    b1 = -(1 + np.cos(w0))
+    b2 = (1 + np.cos(w0)) / 2
+
+    a0 = 1 + alpha
+    a1 = -2 * np.cos(w0)
+    a2 = 1 - alpha
+
+    #normalization 
+    a1 = a1 / a0
+    a2 = a2 / a0
+    b0 = b0 / a0
+    b1 = b1 / a0
+    b2 = b2 / a0
+
+    return b0, b1, b2, a1, a2
+
+
+#3-1. Band-pass_1(Constant skirt gain BPF)
+# # 중심주파수(peak)가 Q에 영향을 받아서 증폭 
+def calculate_bandpass_coefficients(f0, Q, fs):
+    w0 = 2 * np.pi * (f0/fs)
+
+    alpha = np.sin(w0) / (2 * Q)
+
+    b0 =   np.sin(w0)/2  # =   Q*alpha
+    b1 =   0
+    b2 =  -np.sin(w0)/2  # =  -Q*alpha
+    a0 =   1 + alpha
+    a1 =  -2 * np.cos(w0)
+    a2 =   1 - alpha
+
+    #normalization 
+    a1 = a1 / a0
+    a2 = a2 / a0
+    b0 = b0 / a0
+    b1 = b1 / a0
+    b2 = b2 / a0
+
+    return b0, b1, b2, a1, a2
+
+#3-2. Band-pass_2(Constant 0dB peak gain)
+# 중심주파수(peak)값을 0dB 로 유지함 
+def calculate_bandpass2_coefficients(f0, Q, fs):
+    w0 = 2 * np.pi * (f0/fs)
+
+    alpha = np.sin(w0) / (2 * Q)
+
+    b0 =   alpha
+    b1 =   0
+    b2 =  -alpha
+    a0 =   1 + alpha
+    a1 =  -2*np.cos(w0)
+    a2 =   1 - alpha
+
+    #normalization 
+    a1 = a1 / a0
+    a2 = a2 / a0
+    b0 = b0 / a0
+    b1 = b1 / a0
+    b2 = b2 / a0
+
     return b0, b1, b2, a1, a2
 
 
@@ -136,6 +230,51 @@ def plot_impulse_response(output, fs):
     plt.ylim(-60, 20)
     plt.tight_layout()
     plt.show()
+
+
+#comparison : BPF1 vs BPF2
+def comparison_BPF1_BPF2(output1, output2, fs):
+    fig, axes = plt.subplots(1, 2, figsize=(10, 8))
+
+    ax = axes[0]
+
+    spectrum = np.fft.rfft(output1)
+    magnitude = np.abs(spectrum)
+    mag_db = 20 * np.log10(magnitude + 1e-12)
+
+    fft_freq = np.fft.rfftfreq(len(output1), (1/fs))
+
+
+    ax.plot(fft_freq[1:], mag_db[1:])
+    ax.set_title("BPF1 : Constant skirt gain BPF")
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Magnitude (dB)")
+    ax.set_xlim(20, 20000)
+    ax.set_ylim(-60, 20)
+    ax.set_xscale("log")
+
+
+    ax = axes[1]
+
+    spectrum = np.fft.rfft(output2)
+    magnitude = np.abs(spectrum)
+    mag_db = 20 * np.log10(magnitude + 1e-12)
+
+    fft_freq = np.fft.rfftfreq(len(output2), (1/fs))
+
+
+    ax.plot(fft_freq[1:], mag_db[1:])
+    ax.set_title("BPF2 : Constant 0dB peak gain")
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_ylabel("Magnitude (dB)")
+    ax.set_xlim(20, 20000)
+    ax.set_ylim(-60, 20)
+    ax.set_xscale("log")
+
+
+    plt.tight_layout()
+    plt.show()
+    
 
 
 
